f8588208e38ab085a93cc881346ae4ab2f5bd4fd
Magnus Karlsson <m.karlsson@outlook.com>
2015-12-20 18:04:43 +0100

    initial

---
+++ src/PostIFormFileToBlobStorage/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Mvc;
+using PostIFormFileToBlobStorage.Services;
+using Microsoft.Net.Http.Headers;
+
+namespace PostIFormFileToBlobStorage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task PostImage(IList<IFormFile> files)
+        {
+            foreach (var file in files)
+            {
+                var fileName = ContentDispositionHeaderValue
+                .Parse(file.ContentDisposition)
+                .FileName
+                .Trim('"');// FileName returns "fileName.ext"(with double quotes) in beta 3
+                var imageFile = file;
+                if (fileName.EndsWith(".jpg") || fileName.EndsWith(".png"))// Important for security if saving in webroot
+                {
+                    await UpdateAd(imageFile, fileName);
+                }
+            }
+        }
+        public async Task UpdateAd(IFormFile imageFile, string fileName)
+        {
+            var _blobStorage = new BlobStorage();
+            await _blobStorage.UploadAndSaveBlobAsync(imageFile, fileName);
+        }
+        
+    }
+}
---
+++ src/PostIFormFileToBlobStorage/Services/BlobStorage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+
+namespace PostIFormFileToBlobStorage.Services
+{
+    public class BlobStorage 
+    {
+        CloudBlobClient _blobClient;
+        CloudBlobContainer _container;
+
+        public BlobStorage()
+        {
+            
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=NAMEOFSTORAGE;AccountKey=YOURSTORAGEKEY");
+
+            // Create a blob client.
+            _blobClient = storageAccount.CreateCloudBlobClient();
+            _blobClient.DefaultRequestOptions.RetryPolicy = new LinearRetry(TimeSpan.FromSeconds(3), 3);
+
+            // Get a reference to a container named “mycontainer.”
+            _container = _blobClient.GetContainerReference("images");
+
+            // If “mycontainer” doesn’t exist, create it.
+            _container.CreateIfNotExistsAsync();
+        }
+
+        public async Task UploadAndSaveBlobAsync(IFormFile imageFile, string fileName)
+        {
+            
+            await _container.CreateIfNotExistsAsync();
+            await _container.SetPermissionsAsync(new BlobContainerPermissions
+            {
+                PublicAccess = BlobContainerPublicAccessType.Blob
+            });
+            //Get reference to a blob
+
+            var blockBlob = _container.GetBlockBlobReference(fileName);
+            blockBlob.Properties.ContentType = imageFile.ContentType;
+
+            try
+            {
+                using (var stream = imageFile.OpenReadStream())
+                {
+                    await blockBlob.UploadFromStreamAsync(stream);
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError("Could not upload image" + e.Message);
+                throw;
+            }
+
+            var blobsName = blockBlob.Uri.ToString();
+           
+        }
+
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        public async Task<bool> AddToBlob(string folder, string name)
+        {
+
+            // Get a reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = _container.GetBlockBlobReference("myblob");
+
+            // Create or overwrite the "myblob" blob with the contents of a local file
+            // named “myfile”.
+            using (var fileStream = File.OpenRead(@"path\myfile"))
+            {
+                await blockBlob.UploadFromStreamAsync(fileStream);
+            }
+            return true;
+        }
+
+    }
+}
